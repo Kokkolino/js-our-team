7b1e1807ce1780735a18c77f4be70ce7c7f684e6
Francesco C <fran461j@outlook.it>
2022-11-06 13:01:01 +0100

    switch

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+for (let key in team) {
+
+    for(let i = 0; i<3; i++){
+        switch(i) {
+            case 0:
+                console.log(team[key].name)
+                break;
+            case 1:
+                console.log(team[key].role)
+                break;
+            case 2:
+                console.log(team[key].image)
+                break;
+        }
+    }
+}
